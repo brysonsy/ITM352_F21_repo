c2d00a43124ee4b7fa74440385a42d057fadb692
brysonsy <brysonsy@hawaii.edu>
2021-10-06 12:54:06 -1000

    Update Ex1.js

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -2,9 +2,9 @@ require("./products_data.js");
 
 var num_products = 5;
 for (var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
-    console.log(`${count}. ${eval('name' + count)}`);
     if (count > num_products/2) {
         console.log("That's enough!");
         process.exit();
     }
+    console.log(`${count}. ${eval('name' + count)}`);
 }

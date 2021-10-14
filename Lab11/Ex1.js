4e78ccc300e15683f4bc19e45ecd7050461dc17a
brysonsy <brysonsy@hawaii.edu>
2021-10-13 18:32:39 -1000

    Wod Done

---
+++ Lab11/Ex1.js
@@ -0,0 +1,10 @@
+var num1 = 5;
+var num2 = 2;
+var answer = num1 / num2;
+var xxx = function() {
+    answer.toFixed(2);
+  };
+console.log(roundedAnswer);
+
+
+
--- WODs/SmartPhoneProducts3/products_data.js
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -1,21 +1,27 @@
-var name1 = "HTC";
-var price1 = 40.00;
-var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+product1 = {
+brand : "HTC",
+price : 40.00,
+image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+},
+product2 = {
+    brand : "Apple",
+    price : 75.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+},
+product3 = {
+    brand : "Nokia",
+    price : 35.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+},
+product4 = {
+    brand : "Samsung",
+    price : 45.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+},
+product5 = {
+    brand : "Blackberry",
+    price : 10.00,
+    image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+}
 
-var name2 = "Apple";
-var price2 = 75.00;
-var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
-
-var name3 = "Nokia";
-var price3 = 35.00;
-var image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
-
-var name4 = "Samsung";
-var price4 = 45.00;
-var image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
-
-var name5 = "Blackberry";
-var price5 = 10.00;
-var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
-
- console.log("Product variables loaded...");
+products = [product1, product2, product3, product4, product5]

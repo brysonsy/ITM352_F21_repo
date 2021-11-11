f82619a337f7a4815699f8d63991d25faa5344b8
brysonsy <brysonsy@hawaii.edu>
2021-11-10 22:26:52 -1000

    Update Assignment1

---
+++ Yuen_Bryson_Assignment1/public/products_data.js
@@ -0,0 +1,39 @@
+// Product Objects
+
+var products = [ 
+    {
+        "brand" : "Poke Ball",
+        "image" : "./images/pokeball.png",
+        "price" : 2.00,
+        "desc" : "It has a simple red and white design, and it's the most known kind of Poke Ball.",
+        "quantity" : 15
+    },
+    {   
+        "brand" : "Great Ball",
+        "image" : "./images/greatball.png",
+        "price" : 5.00,
+        "desc" : "A high-performance ball with a higher catch rate than a standard Poke Ball.",
+        "quantity" : 8
+    },
+    {
+        "brand" : "Ultra Ball",
+        "image" : "./images/ultraball.png",
+        "price" : 15.00,
+        "desc" : "An ultra-performance ball with a higher catch rate than a Great Ball.",
+        "quantity" : 5
+    },
+    {
+        "brand" : "Quick Ball",
+        "image" : "./images/quickball.png",
+        "price" : 10.00,
+        "desc" : "A kind of Poke Ball that works better the sooner it is used in battle.",
+        "quantity" : 10
+    },
+    {
+        "brand" : "Dusk Ball",
+        "image" : "./images/duskball.png",
+        "price" : 12.50,
+        "desc" : "A ball that works better in dark places or at night.",
+        "quantity" : 12
+    }
+]
